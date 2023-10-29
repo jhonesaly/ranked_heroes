b64a32add2fc0d46a92fef8bdfe56905bb9762b2
Jhones <jhonesaly@gmail.com>
2023-10-28 23:14:11 -0300

    refactor: comentários

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 import os
 from main_functions import *
 
-os.system('cls')
+## Configurações iniciais
 
+os.system('cls')
 herois = random_hero_league(10)
 
-print("Bem-vindo ao ranking dos heróis!\n Os heróis disponíveis são:")
 
+## Listando heróis
+
+print("Bem-vindo ao ranking dos heróis!\n Os heróis disponíveis são:")
 for heroi in herois:
     print(heroi)
+
+
