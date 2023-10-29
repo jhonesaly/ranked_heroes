81d2bd11629531f52f71ae55f141b5d21996b98a
Jhones <jhonesaly@gmail.com>
2023-10-28 23:11:30 -0300

    feat: programa mostra lista de herois

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # Base da aplicação
 from main_functions import *
 
-print(random_hero_name())
+herois = random_hero_league(10)
+
+print("Bem-vindo ao ranking dos heróis!\n Os heróis disponíveis são:")
+
+for heroi in herois:
+    print(heroi)
