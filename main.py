42c5759f02c900172de7359df641ac660d0fa789
Jhones <jhonesaly@gmail.com>
2023-10-30 11:29:46 -0300

    refactor: reduzindo código

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ from main_functions import *
 
 ## Configurações iniciais
 
-os.system('cls')
+print("Bem-vindo ao ranking dos heróis!")
 herois = random_hero_league(10)
-
-
-## Listando heróis
-
-print("Bem-vindo ao ranking dos heróis!\nOs heróis disponíveis são:")
-for heroi in herois:
-    print(heroi[0])
-
-
-## Input de herói escolhido
 
 while True: 
     
+    os.system('cls')
+
+    ## Listando heróis
+    print("Os heróis disponíveis são:")
+    for heroi in herois:
+        print(heroi[0])
+
+    ## Input de herói escolhido
     heroi_escolhido = input("Por favor, escolha um herói da lista para saber o seu nível: ")
 
     ## Verifica se escolha está na lista
@@ -37,14 +35,13 @@ while True:
         print(f"O Herói de nome **{heroi_escolhido}** está no nível de **{nivel_heroi}**")
 
     else:
-        print("Herói não está na lista.")
-        novo_heroi = input("Deseja adicionar um novo herói? [s/n] ")
+        print("Esse herói não está na lista.")
+        novo_heroi = input("Deseja adicioná-lo? [s/n] ")
         
         if novo_heroi.lower() == 's':
-            novo_nome = input("Digite o nome do herói: ")
-            novo_xp = int(input(f"Digite o xp do herói {novo_nome}: "))
-            herois.append([novo_nome, novo_xp])
-            
+            novo_xp = int(input(f"Digite o xp do herói {heroi_escolhido}: "))
+            herois.append([heroi_escolhido, novo_xp])
+
             continue
         else:
             continue
