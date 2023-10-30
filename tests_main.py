4935ef9b789b46647e29590e61d6b981d7531644
Jhones <jhonesaly@gmail.com>
2023-10-30 11:34:33 -0300

    test: função de criar xp

--- tests_main.py
+++ tests_main.py
@@ -8,3 +8,9 @@ class TestMainFunctions(unittest.TestCase):
         self.assertTrue(name.isalpha())  # Verifica se o nome contém apenas letras
         self.assertTrue(name[0].isupper())  # Verifica se a primeira letra é maiúscula
 
+    def test_random_hero_xp(self):
+        min_number = 1
+        max_number = 11000
+        xp = random_hero_xp(min_number, max_number)
+        self.assertTrue(min_number <= xp <= max_number)  # Verifica se o xp está dentro do intervalo especificado
+
